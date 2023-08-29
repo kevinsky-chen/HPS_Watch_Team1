d94d0902b5372006741a9a473411d8074ac0dde1
kevinsky-chen <56266480+kevinsky-chen@users.noreply.github.com>
2023-08-29 13:52:35 +0800

    Create motor.py

---
+++ Device/code/motor.py
@@ -0,0 +1,20 @@
+from machine import Pin, PWM
+from time import sleep
+class Motor:
+    def __init__(self, pinNo):
+        self.gpio = pinNo
+        self._on = False
+        self.speed=0
+        self.pwm1=PWM(Pin(pinNo))
+        self.pwm1.freq(2000)
+        self.pwm1.duty_u16(0)
+    def setSpeed(self,s):
+        self._on=True
+        self.speed=s
+        self.pwm1.duty_u16(int(65535*s/100))    
+    def off(self):
+        self._on=False
+        self.pwm1.duty_u16(0)    
+    def on(self):
+        self._on=True
+        self.pwm1.duty_u16(int(65535*self.speed/100))
